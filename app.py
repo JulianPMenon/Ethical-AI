a27e4d39fc6fd838cbab4cfb6d8b6b77eee0c504
JulianPMenon <97676461+JulianPMenon@users.noreply.github.com>
2024-12-11 16:57:48 +0100

    spelling

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
     
     if choice == "Home":
-        st.subheader("Check an argument for thair Approprietness")
+        st.subheader("Check an argument for Inapproprietness")
         
         with st.form(key='myform'):
             raw_text = st.text_area("Input an argument here.")
